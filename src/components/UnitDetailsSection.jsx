e75b8dfc66840d658a5d541328164fd212dc0172
sankeerth-m-p <sankeerthajith3@gmail.com>
2026-02-04 16:39:27 +0530

    Style : cards ui width dajusted

--- src/components/UnitDetailsSection.jsx
+++ src/components/UnitDetailsSection.jsx
@@ -72,7 +72,7 @@ export default function UnitDetailsSection() {
         >
             {/* Background Image - Sticky/Parallax */}
             <div
-                className="fixed top-0 left-0 w-full h-screen bg-cover bg-center -z-10"
+                className="sticky top-0 left-0 w-full h-screen bg-cover bg-center -z-10"
                 style={{
                     backgroundImage: `url('/herobackgorund.webp')`,
                     backgroundSize: 'cover',
@@ -97,7 +97,7 @@ export default function UnitDetailsSection() {
 
 
                     <div className="flex-1   min-h-0 mt-2">
-                        <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-6 max-w-7xl mx-auto h-full overflow-auto md:overflow-hidden">
+                        <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-6 max-w-5xl mx-auto h-full overflow-auto md:overflow-hidden">
                             {units.map((unit, idx) => (
                                 <FloatUpText key={unit.id} delay={idx * 0.2} yMultiplier={2}>
                                     <TiltCard>
