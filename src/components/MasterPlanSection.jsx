13416415f340045abeb3aa9041e5db3d7799c842
sankeerth-m-p <sankeerthajith3@gmail.com>
2026-02-12 15:52:25 +0530

    plost thing has been moved colser to the center

--- src/components/MasterPlanSection.jsx
+++ src/components/MasterPlanSection.jsx
@@ -65,7 +65,7 @@ const MasterPlanSection = () => {
 
           {/* RIGHT SIDE: Masterplan image (STAYS FIXED) */}
           <  FloatUpText
-            className="w-full lg:absolute lg:right-[120px] lg:bottom-0 lg:w-[900px]
+            className="w-full lg:absolute lg:right-[200px] lg:bottom-0 lg:w-[900px]
            mt-8 sm:mt-10 lg:mt-0"
        
           >
